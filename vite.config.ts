a5ac3b2f379db96255d74b1ad3295c57fba7add8
Karn Wong <karn@karnwong.me>
2024-08-03 18:34:16 +0700

    set baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/python-playground/",
+  base: "https://python.karnwong.me/", // https://github.com/vitejs/vite/discussions/13910#discussioncomment-6832557
   plugins: [
     compression(),
     react(),
